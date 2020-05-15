d2647650f60cbeb959169efc108c53c68eb58a2e
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2020-05-15 22:05:38 +0300

    Add mu4e and tweak some colors

--- init.el
+++ init.el
@@ -166,8 +166,8 @@
        ;;yaml              ; JSON, but readable
 
        :email
-       ;;(mu4e +gmail)
-       ;;notmuch
+       (mu4e +gmail)
+       notmuch
        ;;(wanderlust +gmail)
 
        :app
--- modules/custom/ui/config.el
+++ modules/custom/ui/config.el
@@ -1,7 +1,7 @@
 ;;; custom/ui/config.el -*- lexical-binding: t; -*-
 
 (custom-set-faces!
-  '(default :background "#1d1f21")
+  '(default :background "#0d1117")
   '(mode-line :background "#2c2e34")
   '(window-divider :foreground "#2c2e34")
   '(treemacs-directory-face :foreground "#51afef"))
