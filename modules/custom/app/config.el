6e4d321c0e4134efb4b51c84639f34a3166a3a72
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2021-05-27 18:05:06 +0300

    oops

--- modules/custom/app/config.el
+++ modules/custom/app/config.el
@@ -9,6 +9,6 @@
   (slack-register-team
     :name "houston-inc"
     :default t
-    :token "xoxs-243295913552-1858796528662-1852661758583-e51665d8493d5b345d1fd07f5e2afb03d95e651ee9db63bf1bcb7f0600008391"
+    :token ""
     :subscribed-channels '(logisnext)
     :full-and-display-names t))
