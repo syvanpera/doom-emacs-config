50c5daeaec670d904a48c425bda0f5fe6483efdb
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2022-06-15 11:05:27 +0300

    Initial commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2998f96e5bda8a31cf3eb07d87aa26ee27355fc45634e6508bb0e2c71a65b90a" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(vertical-border ((t (:background "#282828" :foreground "#282828")))))
